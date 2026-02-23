4b9ddd1ce99a8ba215e45e5c52ef5746b19ef42f
James Mwangi K <jameshmwangiy@gmail.com>
2026-02-23 10:05:58 +0300

    Implemented Active Job, Action mailer and Active Storage

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  before_action :correct_user, only: [:show, :edit, :update]
+  skip_before_action :login_required, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      UserMailer.with(to: @user.email, name: @user.name).welcome.deliver_later(wait: 1.minutes) 
+      log_in(@user)
+      redirect_to user_path(@user.id), notice: 'アカウントを登録しました。'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to current_user unless current_user?(@user)
+  end
+end
